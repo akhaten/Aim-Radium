d833988eadf10f77620942cc28db96624d6efa5f
akhaten <khafifjessy@gmail.com>
2022-07-18 15:40:04 +0200

    clean

--- build/CMakeFiles/AimRadium_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "AimRadium_autogen/mocs_compilation.cpp"
-  "CMakeFiles/AimRadium_autogen"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/AimRadium_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/Install_AimRadium.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Install_AimRadium"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Install_AimRadium.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
